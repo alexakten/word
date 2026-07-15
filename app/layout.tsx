4297e3531013a343a590580f6855f4b96712da60
Alex Aktén <alex.akten@outlook.com>
2026-07-15 18:39:11 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Spellsurf",
   description: "Discover and create new words.",
+  icons: {
+    icon: [{ url: "/favicon.png", type: "image/png" }],
+    apple: [{ url: "/favicon.png", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({
